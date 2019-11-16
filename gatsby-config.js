2314329be6253be5cae5230f7f16e814dcd63346
lexifdev <sl@lxf.kr>
2019-11-16 11:01:37 +0900

    init

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pofol Dev Starter`,
+    description: `포폴 데브 워크샵에서 사용하는 기본 프로젝트 입니다.`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`
+  ],
+}
