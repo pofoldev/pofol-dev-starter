a0a84f5dd885b8f4ec9d89c5937fcc2d886d3d39
lexifdev <sl@lxf.kr>
2019-11-17 22:34:08 +0900

    과정중에 배웠던 문법으로 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 
-const Index = () => (
-    <div>
-        Hello!
-    </div>
-)
+const Index = () => {
+    return (
+        <div>
+            Hello!
+        </div>
+    )
+}
 
 export default Index
